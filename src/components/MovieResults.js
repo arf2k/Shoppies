c108e436d58f6ba07dfea1acfa8fcfb78fa49263
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-01-17 12:41:40 -0500

    functions folder added

--- src/components/MovieResults.js
+++ src/components/MovieResults.js
@@ -1,33 +1,35 @@
 import React from "react";
-import MovieCard from './MovieCard.js'
-import styled from 'styled-components'
-
+import MovieCard from "./MovieCard.js";
+import styled from "styled-components";
 
 const MovieResults = (props) => {
+  let renderedMovies;
+  if (props.movieResults) {
+    renderedMovies = props.movieResults.map((movie) => {
+      return (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          addNominee={props.addNominee}
+          parent={"results"}
+          count={props.count}
+        />
+      );
+    });
+  } else {
+    console.log("loading");
+  }
 
-
-     let renderedMovies;
-     if (props.movieResults) {
-       renderedMovies = props.movieResults.map(movie => {
-         return <MovieCard key={movie.id} movie={movie} addNominee={props.addNominee} parent={"results"} count={props.count}  />})
-     } else {
-       console.log("loading")
-     }
-
-
-return (
-     <>
-     <Title>Movie Selections</Title>
-     {renderedMovies}
-     </>
-     
-)
-
-
-}
+  return (
+    <>
+      <Title>Movie Selections</Title>
+      {renderedMovies}
+    </>
+  );
+};
 
 export default MovieResults;
 
 const Title = styled.h1`
-text-align: center
-`
+  text-align: center;
+`;
