455a104656f1819e9fea739b88105a56bf3c6a56
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-05 14:34:20 -0500

    disconnected from netlify so rehidkey

--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -5,7 +5,6 @@ import axios from "axios";
 import Nominations from "../components/Nominations.js";
 import styled from "styled-components";
 
-let key = "11f2c5e1"
 
 const MovieContainer = () => {
   const [searchTerm, setSearchTerm] = useState("");
